f029fb14ee8542e740f013e842755becd0df4b87
Joey Suen <suenjoey@gmail.com>
2021-04-22 03:40:46 +0000

    refactor route

--- app/actions.rb
+++ app/actions.rb
@@ -7,8 +7,13 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 
+    
+
+
+
+
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -21,7 +26,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -34,7 +39,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -47,9 +52,24 @@ get '/' do
     }]
   }
 
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
+
+
+
+
+
+
 
 end
+
+
+
+
+
+
+
 
     # i = 1
     # num = 101
