70cb1b036a1b850bdf248eccd658278bcfbbc0a6
Joey Suen <suenjoey@gmail.com>
2021-04-29 03:20:49 +0000

    added signup

--- app/actions.rb
+++ app/actions.rb
@@ -5,3 +5,25 @@ get '/' do
     erb(:index)
 end
 
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
+
+end
